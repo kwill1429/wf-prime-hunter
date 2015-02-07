10217d8ca3efb0fde717f6986e8cdc6c43196257
David Filipidisz <dfilipidisz@gmail.com>
2015-02-07 19:36:41 +0100

    Update game version to 15.13.6

--- public/js/record.js
+++ public/js/record.js
@@ -218,7 +218,7 @@ $(document).ready(function() {
       $('.action-result').empty();
       $('.choose-multiple-reward').empty().addClass('hidden');
       
-      $("#version").val("15.13.2");
+      $("#version").val("15.13.6");
     },
     onChooseMultipleReward: function(evt) {
       evt.preventDefault();
