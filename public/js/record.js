ae1f1fb5af0aa95c50f635ae9c001cf2ca46cfc9
David Filipidisz <dfilipidisz@gmail.com>
2015-04-06 18:03:53 +0200

    Update game version to 16.2.1

--- public/js/record.js
+++ public/js/record.js
@@ -218,7 +218,7 @@ $(document).ready(function() {
       $('.action-result').empty();
       $('.choose-multiple-reward').empty().addClass('hidden');
       
-      $("#version").val("16.2.0");
+      $("#version").val("16.2.1");
     },
     onChooseMultipleReward: function(evt) {
       evt.preventDefault();
